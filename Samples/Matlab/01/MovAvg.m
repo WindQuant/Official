7df6a9f23074b42fa05c91175875a854a0b6643d
WindQuant <service@dajiangzhang.com>
2014-12-29 13:12:28 +0800

    重新整理文件夹。 添加C#,C++接口 库及例子。

--- Samples/Matlab/01/MovAvg.m
+++
@@ -1,7 +0,0 @@
-function ave = MovAvg(Prices,I,length)
-if(length>I)
-   ave = sum(Prices(1:I))/I; 
-else
-   ave = sum(Prices(I-length+1:I))/length; 
-end
-end
--- Samples/Matlab/01/Summary.m
+++
@@ -1,4 +0,0 @@
-function Asset = Summary(DB,Asset)
-asset = Asset.Position.*DB.Close + Asset.Cash;
-plot(asset);
-title('总资产曲线（初始为0）')
